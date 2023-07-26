e2cbb4d81469e0b2bc7f5a9a5820303dd0ee49fa
Sittakon phommee <Sittakon_phomme@cmu.ac.th>
2023-07-26 13:09:47 +0700

    Update StickInThePileGamerepo.py

--- StickInThePileGamerepo.py
+++ StickInThePileGamerepo.py
@@ -65,34 +65,6 @@ while stick_pile != 0:
         print("I WON (Python WON)")
         break
     
-    #     ## take stick from pile 
-    # stick_pile = stick_pile - hmtake
-    # ## check if not enough stick to take
-    # if stick_pile<0:
-    #     print("There are no enough sticks to take.")
-    #     stick_pile = stick_pile + hmtake
-    #     ## check win condition for computer
-    # elif stick_pile == 0:          
-    #     print(name,", takes the last stick.")
-    #     print("I, smart computer, win !!!!")
-    #     break
-    # else: 
-    #     print("There are ",stick_pile," sticks in the pile.")
-    #     stick_pile = stick_pile - ct ## take stick from the pile
-    # ## start to check condition of smart computer
-    #     if stick_pile == 2: ##condition to make the computer smart
-    #         stick_pile = stick_pile + ct -1
-    #         ct = 1
-    #         print("I, smart computer, takes : ",ct)
-    #         print("There are ",stick_pile," sticks in the pile.") 
-    #     elif stick_pile<=0: ##check win condition for player
-    #             print("I, smart computer, takes the last stick.")
-    #             print(name," win ( I, smart computer, am sad T_T)")
-    #             break
-    #     else:## show howmany sticks that smart compuet take
-    #         print("I, smart computer, takes : ",ct)
-    #         print("There are ",stick_pile," sticks in the pile.") 
-
         
 
 #sittakon phommee 660631100
